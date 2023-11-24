5e17d03b1fc5701c0a9ec610e8dcd48e6eee79ad
Dipra <dipradas5940@gmail.com>
2023-11-24 16:48:52 +0600

    49.3 - Sidebar & Content Devided and Optimized

--- src/app/(withlayout)/student/page.tsx
+++ src/app/(withlayout)/student/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StudentPage = () => {
     return (
         <div>
-            <h1>Student Page</h1>
+            <h1 style={{color:'blue'}}>Student Page</h1>
         </div>
     );
 };
---
+++ src/app/(withlayout)/super-admin/page.tsx
@@ -0,0 +1,9 @@
+const SuperAdmin = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>i m super admin</h1>
+        </div>
+    );
+}; 
+
+export default SuperAdmin;
